e356af16c15cfe44fac61f8e8b5f45d43682023c
Okman-0920 <nohoksun94@gmail.com>
2024-12-12 16:16:45 +0900

    setting

---
+++ src/main/java/com/ll/labspring20241212/LabSpring20241212Application.java
@@ -0,0 +1,13 @@
+package com.ll.labspring20241212;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSpring20241212Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabSpring20241212Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/labspring20241212/LabSpring20241212ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.labspring20241212;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSpring20241212ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
